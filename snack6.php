985825496d4f970e391fbcf7b915ce65a9920907
DanyMencu <dany69men@gmail.com>
2021-12-17 16:47:04 +0100

    Snack 6 complete

--- snack6.php
+++ snack6.php
@@ -13,6 +13,45 @@
     <?php
     //Snack 6
     //Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
+
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+
+    $teachers = $db['teachers'];
+
+    for ($i = 0; $i < count($teachers); $i++) {
+        echo '<div style="background-color: gray">'."Name: " . $teachers[$i]['name'] . "<br>"
+        . "Lastname: " . $teachers[$i]['lastname'] . "<br>"
+        . "<br></div>";
+    };
+
+    $pm = $db['pm'];
+
+    for ($i = 0; $i < count($pm); $i++) {
+        echo '<div style="background-color: green">'."Name: " . $pm[$i]['name'] . "<br>"
+        . "Lastname: " . $pm[$i]['lastname'] . "<br>"
+        . "<br></div>";
+    };
     ?>
 
 </body>
