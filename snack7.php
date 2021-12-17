46e78dbcb91d061a5252edd892bcdd4d066ad967
DanyMencu <dany69men@gmail.com>
2021-12-17 17:08:51 +0100

    Snack 7 complete

--- snack7.php
+++ snack7.php
@@ -13,6 +13,41 @@
     <?php
     //Snack 7
     //Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno.
+
+    $studentsList = [
+        'student1' => [
+            'name' => 'Giggi',
+            'lastname' => 'Dag',
+            'votes' => [8, 7, 8, 6, 7, 7, 8, 9]
+        ],
+        'student2' => [
+            'name' => 'Paul',
+            'lastname' => 'Karkbrenner',
+            'votes' => [10, 9, 8, 9]
+        ],
+        'student3' => [
+            'name' => 'Pippo',
+            'lastname' => 'Baudo',
+            'votes' => [4, 3, 5, 2, 10, 9]
+        ],
+        'student4' => [
+            'name' => 'Claudio',
+            'lastname' => 'Bisio',
+            'votes' => [10]
+        ],
+    ];
+
+    $studentsKeys = array_keys($studentsList);
+
+    for ($i = 0; $i < count($studentsList); $i++) {
+        $average = array_sum($studentsList[$studentsKeys[$i]]['votes']) / count($studentsList[$studentsKeys[$i]]['votes']);
+        echo "<strong>" . $studentsKeys[$i] . "</strong>" . "<br><br>";
+
+        echo "Name: " . $studentsList[$studentsKeys[$i]]['name'] . "<br>"
+            . "Lastname: " . $studentsList[$studentsKeys[$i]]['lastname'] . "<br>"
+            . "Votes: " . $average
+            . "<br><br>";
+    }
     ?>
 
 </body>
