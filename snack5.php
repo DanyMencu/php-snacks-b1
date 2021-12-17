f28e6b193b7b6ce48b0aa45b3d6c98f3d51e00dc
DanyMencu <dany69men@gmail.com>
2021-12-17 17:18:49 +0100

    Snack 5 complete

--- snack5.php
+++ snack5.php
@@ -13,6 +13,16 @@
     <?php
     //Snack 5
     //Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
+
+    $paragraph = "È giunta l’ora di rimarginare le ferite. È giunta l’ora di colmare i divari che ci dividono. Questo è il tempo di costruire. Abbiamo finalmente raggiunto l’emancipazione politica. Ci impegniamo a costruire una pace completa, giusta e durevole. Assumiamo ufficialmente il compito di costruire una società in cui tutti i sudafricani, neri e bianchi, potranno camminare a testa alta, senza alcun timore, certi del loro inalienabile diritto alla dignità umana. Una nazione di tutti i colori, in pace con se stessa e con il mondo. Socrate, Apologia";
+
+    $splitParagraph = explode('.', $paragraph);
+    echo "<strong>Originale: </strong>" .$paragraph. "<br><br>";
+
+    echo "<strong> Diviso in paragrafi: </strong> <br>";
+    for ($i = 0; $i < count($splitParagraph); $i++) {
+        echo $splitParagraph[$i] . "<br>";
+    }
     ?>
 
 </body>
